00561cc4694c5d45ab56c3b710df7b0a36b4fab7
nnp2302 <65790507+nnp2302@users.noreply.github.com>
2023-03-25 12:27:18 +0700

    Update GiaoHang.sql

--- Others/GiaoHang.sql
+++ Others/GiaoHang.sql
@@ -1,4 +1,6 @@
-﻿create table [Quyền](
+﻿create database [Giao Hàng]
+use [Giao Hàng]
+create table [Quyền](
 	[Mã quyền] int primary key,
 	[Tên quyền] nvarchar(1000)
 )
@@ -19,4 +21,15 @@ create table [Phân chức năng](
 	[Mã quyền] int,
 	[Mã chức năng] int,
 	primary key([Mã quyền],[Mã chức năng])
+)
+create table [Nhân viên](
+	[Mã nhân viên] int primary key,
+	[Tên nhân viên] nvarchar(100),
+	[Mã chức vụ] int
+)
+create table [Tài khoản](
+	[Mã nhân viên] int primary key,
+	[Tài khoản] varchar(100),
+	[Mật khẩu] varchar(100),
+	foreign key ([Mã nhân viên]) references [Nhân viên]([Mã nhân viên])
 )
